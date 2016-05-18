9830f5313272aee2e09bb06f220d00c82adb8981
Brooke <brooke.nicole.angel@gmail.com>
2016-05-17 17:50:05 -0700

    New homeworks (#1)
    * add rspec homework
    
    * fix rspec homework zip files
    
    * typo fix

---
+++ ruby/homeworks/solutions/rspec-homework/spec/dessert_spec.rb
@@ -0,0 +1,61 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:brownie) { Dessert.new("brownie", 100) }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(brownie.type).to eq("brownie")
+    end
+
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(100)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cake", "tons") }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      brownie.add_ingredient("chocolate")
+      expect(brownie.ingredients).to include("chocolate")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["chocolate", "flour", "egg", "sugar", "butter"]
+
+      ingredients.each do |ingredient|
+        brownie.add_ingredient(ingredient)
+      end
+
+      expect(brownie.ingredients).to eq(ingredients)
+      brownie.mix!
+      expect(brownie.ingredients).not_to eq(ingredients)
+      expect(brownie.ingredients.sort).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      brownie.eat(20)
+      expect(brownie.quantity).to eq(80)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { brownie.eat(120)}.to raise_error("not enough left!")
+    end
+  end
+end
