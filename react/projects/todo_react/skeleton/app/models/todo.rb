5cf5b5730d2d84984bd4d52de8429fe52cf239e7
Ken Tran <loschorts@gmail.com>
2016-05-24 09:27:30 -0700

    remove extraneous skeleton folder from todo_react

--- react/projects/todo_react/skeleton/app/models/todo.rb
+++
@@ -1,5 +0,0 @@
-class Todo < ActiveRecord::Base
-  validates :title, presence: true
-  validates :done, inclusion: [true, false], default: false
-  after_initialize { self.done = false if self.done.nil? }
-end
--- react/projects/todo_react/skeleton/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'static_pages#root'
-  namespace :api, defualts: { format: :json } do
-    resources :todos, except: [:new, :edit]
-  end
-end
