743f50ea7df952398516b303ee16003297894fc8
sewinter <samuelewinter@gmail.com>
2016-05-29 19:15:35 -0700

    es6ify hanoi
    Former-commit-id: acb1bb5f68878f6e76b2f29ef2fafb038791efc2

--- javascript/projects/hanoi_node/solution/game.js
+++ javascript/projects/hanoi_node/solution/game.js
@@ -1,66 +1,68 @@
-function Game () {
-  this.towers = [[3, 2, 1], [], []];
-};
-
-Game.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
-  var startTower = this.towers[startTowerIdx];
-  var endTower = this.towers[endTowerIdx];
-
-  if (startTower.length === 0) {
-    return false;
-  } else if (endTower.length == 0) {
-    return true;
-  } else {
-    var topStartDisc = startTower[startTower.length - 1];
-    var topEndDisc = endTower[endTower.length - 1];
-    return topStartDisc < topEndDisc;
+class Game {
+  constructor() {
+    this.towers = [[3, 2, 1], [], []];
   }
-};
 
-Game.prototype.isWon = function () {
-  // move all the discs to the last or second tower
-  return (this.towers[2].length == 3) || (this.towers[1].length == 3);
-};
+  isValidMove(startTowerIdx, endTowerIdx) {
+    let startTower = this.towers[startTowerIdx];
+    let endTower = this.towers[endTowerIdx];
 
-Game.prototype.move = function (startTowerIdx, endTowerIdx) {
-  if (this.isValidMove(startTowerIdx, endTowerIdx)) {
-    this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
-    return true;
-  } else {
-    return false;
-  }
-};
-
-Game.prototype.print = function () {
-  console.log(JSON.stringify(this.towers));
-};
-
-Game.prototype.promptMove = function (reader, callback) {
-  this.print();
-  reader.question("Enter a starting tower: ", function (start) {
-    var startTowerIdx = parseInt(start);
-    reader.question("Enter an ending tower: ", function (end) {
-      var endTowerIdx = parseInt(end);
-      callback(startTowerIdx, endTowerIdx)
-    });
-  });
-};
-
-Game.prototype.run = function (reader, gameCompletionCallback) {
-  this.promptMove(reader, (function (startTowerIdx, endTowerIdx) {
-    if (!this.move(startTowerIdx, endTowerIdx)) {
-      console.log("Invalid move!");
-    }
-
-    if (!this.isWon()) {
-      // Continue to play!
-      this.run(reader, gameCompletionCallback);
+    if (startTower.length === 0) {
+      return false;
+    } else if (endTower.length == 0) {
+      return true;
     } else {
-      this.print();
-      console.log("You win!");
-      gameCompletionCallback();
+      let topStartDisc = startTower[startTower.length - 1];
+      let topEndDisc = endTower[endTower.length - 1];
+      return topStartDisc < topEndDisc;
     }
-  }).bind(this));
-};
+  }
 
-module.exports = Game;
+  isWon() {
+    // move all the discs to the last or second tower
+    return (this.towers[2].length == 3) || (this.towers[1].length == 3);
+  }
+
+  move(startTowerIdx, endTowerIdx) {
+    if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+      this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  print() {
+    console.log(JSON.stringify(this.towers));
+  }
+
+  promptMove(reader, callback) {
+    this.print();
+    reader.question("Enter a starting tower: ", start => {
+      let startTowerIdx = parseInt(start);
+      reader.question("Enter an ending tower: ", end => {
+        let endTowerIdx = parseInt(end);
+        callback(startTowerIdx, endTowerIdx)
+      });
+    });
+  }
+
+  run(reader, gameCompletionCallback) {
+    this.promptMove(reader, (startTowerIdx, endTowerIdx) => {
+      if (!this.move(startTowerIdx, endTowerIdx)) {
+        console.log("Invalid move!");
+      }
+
+      if (!this.isWon()) {
+        // Continue to play!
+        this.run(reader, gameCompletionCallback);
+      } else {
+        this.print();
+        console.log("You win!");
+        gameCompletionCallback();
+      }
+    });
+  }
+}
+
+export default Game;
