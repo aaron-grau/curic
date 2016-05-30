89a3feae9be8f8f23fab5792056545e2eac7bebe
Tommy Duek <taduek@gmail.com>
2016-05-30 14:52:35 -0400

    Cleanup. Only keep track of errors in ErrorStore
    Former-commit-id: 5773fe7e26df8de9bda1e99cc2b5be81dc8fe423

--- react/projects/bench_bnb/solution/frontend/stores/user_store.js
+++ react/projects/bench_bnb/solution/frontend/stores/user_store.js
@@ -1,26 +1,18 @@
 var AppDispatcher = require('../dispatcher/dispatcher.js');
 var Store = require('flux/utils').Store;
+var FavoriteConstants = require('../constants/favorite_constants');
 
 var UserStore = new Store(AppDispatcher);
 
-var _currentUser, _errors;
+var _currentUser;
 
 UserStore.__onDispatch = function (payload) {
   switch(payload.actionType) {
-    case "LOGIN":
-    	UserStore.login(payload.user);
-      break;
-    case "LOGOUT":
-    	UserStore.logout();
-      break;
-    case "ERROR":
-      UserStore.setErrors(payload.errors);
-      break;
-    case "FAVORITE_RECEIVED":
+    case FavoriteConstants.FAVORITE_RECEIVED:
       UserStore.addFavorite(payload.favorite.benchId);
       UserStore.__emitChange();
       break;
-    case "FAVORITE_REMOVED":
+    case FavoriteConstants.FAVORITE_REMOVED:
       UserStore.removeFavorite(payload.favorite.benchId);
       UserStore.__emitChange();
       break;
@@ -28,39 +20,14 @@ UserStore.__onDispatch = function (payload) {
   UserStore.__emitChange();
 };
 
-UserStore.addFavorite = function(benchId) {
+UserStore.addFavorite = function (benchId) {
   _currentUser.favorite_benches.push(parseInt(benchId));
 };
 
-UserStore.removeFavorite = function(benchId) {
+UserStore.removeFavorite = function (benchId) {
   var benchIdx = _currentUser.favorite_benches.indexOf(parseInt(benchId));
   _currentUser.favorite_benches.splice(benchIdx, 1);
 };
 
-UserStore.login = function(user){
-	_currentUser = user;
-  _errors = null;
-};
-
-UserStore.logout = function(){
-  _currentUser = null;
-  _errors = null;
-};
-
-UserStore.currentUser = function(){
-  if (_currentUser) {
-  	return $.extend({}, _currentUser);
-  }
-};
-
-UserStore.setErrors = function(errors){
-  _errors = errors;
-};
-
-UserStore.errors = function(){
-  if (_errors){
-    return [].slice.call(_errors);
-  }
-};
 
 module.exports = UserStore;
