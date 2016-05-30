25e138ab5297943c0836738a8452f781e471e1d4
Tommy Duek <taduek@gmail.com>
2016-05-30 14:57:39 -0400

    Move favoriting to SessionStore::_currentUser

--- react/projects/bench_bnb/solution/frontend/stores/session_store.js
+++ react/projects/bench_bnb/solution/frontend/stores/session_store.js
@@ -1,6 +1,7 @@
 var AppDispatcher = require('../dispatcher/dispatcher.js');
 var Store = require('flux/utils').Store;
 var SessionConstants = require('../constants/session_constants');
+var FavoriteConstants = require('../constants/favorite_constants');
 
 var SessionStore = new Store(AppDispatcher);
 
@@ -17,6 +18,15 @@ function _logout() {
   _currentUserHasBeenFetched = true;
 }
 
+function _addFavorite(benchId) {
+  _currentUser.favorite_benches.push(parseInt(benchId));
+};
+
+function _removeFavorite(benchId) {
+  var benchIdx = _currentUser.favorite_benches.indexOf(parseInt(benchId));
+  _currentUser.favorite_benches.splice(benchIdx, 1);
+};
+
 SessionStore.__onDispatch = function (payload) {
   switch(payload.actionType) {
     case SessionConstants.LOGIN:
@@ -27,13 +37,20 @@ SessionStore.__onDispatch = function (payload) {
     	_logout();
       SessionStore.__emitChange();
       break;
+    case FavoriteConstants.FAVORITE_RECEIVED:
+      _addFavorite(payload.favorite.benchId);
+      SessionStore.__emitChange();
+      break;
+    case FavoriteConstants.FAVORITE_REMOVED:
+      _removeFavorite(payload.favorite.benchId);
+      SessionStore.__emitChange();
+      break;
   }
 };
 
 SessionStore.currentUser = function () {
 	return $.extend({}, _currentUser);
 };
-
 
 SessionStore.currentUserHasBeenFetched = function () {
   return _currentUserHasBeenFetched;
