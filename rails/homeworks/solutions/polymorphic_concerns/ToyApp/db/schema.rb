03f073d5ca8549532577f147f25619cb58b0aa6b
sewinter <samuelewinter@gmail.com>
2016-06-03 11:14:19 -0700

    finalize w4d5 hw
    Former-commit-id: afd39abb2f1437b2ae9494af1fe5b84d5790db06

--- rails/homeworks/solutions/polymorphic_concerns/ToyApp/db/schema.rb
+++ rails/homeworks/solutions/polymorphic_concerns/ToyApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602041854) do
+ActiveRecord::Schema.define(version: 20160603180735) do
 
   create_table "cats", force: :cascade do |t|
     t.string   "name",       null: false
