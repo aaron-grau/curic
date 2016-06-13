f0e35fa6c970bccfa1964a91cabe85ab8256e254
Max Schram <maxbschram@gmail.com>
2016-06-13 16:26:18 -0700

    Refactor compose component
    Former-commit-id: 5e3ac9f87bb21c2ef1ca11c0a7dbfdab72705cdc

--- javascript/projects/mail/solution/Compose.js
+++ javascript/projects/mail/solution/Compose.js
@@ -1,5 +1,10 @@
 let content = "";
 let Sent = require("./Sent");
+let message = {
+  to: "",
+  subject: "",
+  body: ""
+};
 module.exports = {
   // render: function () {
   //   return (
@@ -30,6 +35,32 @@ module.exports = {
 
     container.appendChild(textarea)
     container.appendChild(send);
+    return container;
+  },
+  render: function () {
+    let jsx = `
+    <p class="new-message-header">New Message</p>
+    <form class="compose-form">
+    <input placeholder='Recipient' name='to' type="text" value='${message.to}'>
+    <input placeholder='Subject' name='subject' type="text" value='${message.subject}'>
+    <textarea name='body' class='body' rows='20'>${message.body}</textarea>
+    <button type="submit" class="btn btn-primary submit-message">Send</button>
+    </form>
+    `;
+    let container = document.createElement("div");
+    container.className = "new-message";
+    container.innerHTML = jsx;
+    container.addEventListener('change', el => {
+      let target = el.target;
+      message[target.name] = target.value;
+    });
+    container.addEventListener('submit', el => {
+      Sent.addSentMessage(message);
+      message.to = "";
+      message.subject = "";
+      message.body = "";
+    });
+    container.query
     return container;
   }
 };
