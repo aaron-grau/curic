4234a99ce3c3c3511ababe6ddb7293dc0454a803
Max Schram <maxbschram@gmail.com>
2016-06-13 13:58:06 -0700

    Refactor components to return DOM elements from render instead of HTML strings

--- javascript/projects/mail/solution/Inbox.js
+++ javascript/projects/mail/solution/Inbox.js
@@ -1,14 +1,28 @@
+let messages = [
+  "Hey there",
+  "$$$$$",
+];
+
 module.exports = {
+  // render: function () {
+  //   return (
+  //     `
+  //     Inbox
+  //     <ul>
+  //       <li>Hi there</li>
+  //       <li>Email</li>
+  //       <li>Message</li>
+  //     </ul>
+  //     `
+  //   );
+  // }
   render: function () {
-    return (
-      `
-      Inbox
-      <ul>
-        <li>Hi there</li>
-        <li>Email</li>
-        <li>Message</li>
-      </ul>
-      `
-    );
+    let container = document.createElement("ul");
+    messages.forEach(message => {
+      let messageEl = document.createElement("li");
+      messageEl.innerText = message;
+      container.appendChild(messageEl);
+    });
+    return container;
   }
 };
