9cfdbe010abcef3cfc8e5eff8aa477ceb39360e2
Max Schram <maxbschram@gmail.com>
2016-06-14 12:32:56 -0700

    Improve styling of messages

--- javascript/projects/mail/solution/Inbox.js
+++ javascript/projects/mail/solution/Inbox.js
@@ -1,6 +1,6 @@
 let messages = [
-  {from: "grandma@mail.com", subject: "Fwd: Fwd: Fwd: Check this out"},
-  {from: "person@mail.com", subject: "zzz"}
+  {from: "grandma@mail.com", subject: "Fwd: Fwd: Fwd: Check this out", body: "Stay at home mom discovers cure for leg cramps. Doctors hate her"},
+  {from: "person@mail.com", subject: "Questionnaire", body: "Take this free quiz win $1000 dollars"}
 ];
 module.exports = {
   renderMessage: function(message) {
@@ -8,7 +8,8 @@ module.exports = {
     messageEl.className = "message";
     messageEl.innerHTML =`
     <span class='from'>${message.from}</span>
-    <span>${message.subject}</span>
+    <span class="subject">${message.subject}</span>
+    <span class="body">${message.body}</span>
     `;
     return messageEl;
   },
