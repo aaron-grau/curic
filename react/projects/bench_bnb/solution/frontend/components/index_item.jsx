16079b3630ccd3c7987dfcdf3c133e7895185657
Ken Tran <loschorts@gmail.com>
2016-06-16 13:39:05 -0700

    fix names on mail project
    Former-commit-id: a4386713522f98664992388496e9c30e87f75575

---
+++ react/projects/bench_bnb/solution/frontend/components/index_item.jsx
@@ -0,0 +1,28 @@
+const React = require('react');
+const hashHistory = require('react-router').hashHistory;
+
+const IndexItem = React.createClass({
+  handleClick() {
+    const benchID = this.props.bench.id;
+    hashHistory.push("benches/" + benchID );
+  },
+
+  render() {
+    const bench = this.props.bench;
+    return (
+        <div className="bench-index-item"
+             onClick={this.handleClick}
+             key={this.props.key}>
+          {bench.description}
+          <br/>
+          Rating: {bench.average_rating || "No reviews yet"}
+          <br/>
+          Number of Likes: {bench.favorite_users.length}
+          <br/>
+          <img src={bench.picture_url}/>
+        </div>
+    );
+  }
+});
+
+module.exports = IndexItem;
---
+++ react/projects/bench_bnb/solution/frontend/util/bench_api_util.js
@@ -0,0 +1,15 @@
+const FilterParamsStore = require('../stores/filter_params_store');
+
+const ApiUtil = {
+  fetchAllBenches(filters, success){
+    $.get('api/benches', filters, success);
+  },
+  createBench(data, success){
+    $.post('api/benches', { bench: data }, success);
+  },
+  createReview(review, success) {
+    $.post('api/reviews', { review }, success);
+  }
+};
+
+module.exports = ApiUtil;
