a47b4e232ba35fa8c6be54121dd63488db59b615
Ken Tran <loschorts@gmail.com>
2016-06-16 16:43:20 -0700

    remove recipes demo
    Former-commit-id: aa53bf8a8df08e711b0891240421b465fb9ad842

--- react/demos/recipes_demo/flux/frontend/entry.jsx
+++
@@ -1,9 +0,0 @@
-var ReactDOM = require('react-dom');
-var React = require('react');
-var Recipes = require('./components/Recipes');
-
-document.addEventListener("DOMContentLoaded", function () {
-  var root = document.querySelector('#root');
-  ReactDOM.render(<Recipes />, root);
-});
-
