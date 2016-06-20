b569777c3d7a469b477960242ee401574819dc32
sewinter <samuelewinter@gmail.com>
2016-06-20 16:23:01 -0700

    make key listeners dependent on organ

--- react/projects/organ_grinder/solution/frontend/OrganGrinder.jsx
+++ react/projects/organ_grinder/solution/frontend/OrganGrinder.jsx
@@ -3,8 +3,6 @@ const ReactDOM = require('react-dom');
 const Organ = require('./components/Organ');
 const $ = require('jquery');
 
-require("./util/KeyListener");
-
 $(function () {
   const root = document.getElementById('root');
   ReactDOM.render(<Organ/>, root);
