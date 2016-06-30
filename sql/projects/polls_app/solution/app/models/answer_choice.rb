85acb253d5a872166f102ed523e12ab0db2a8ea8
Daniel Colson <dcolson@appacademy.io>
2016-06-30 16:33:31 -0400

    Remove inverse_of from w3d4 solutions
    Former-commit-id: 691f16934c21c2598ba291a7087c655a69bc4ee6

--- sql/projects/polls_app/solution/app/models/answer_choice.rb
+++ sql/projects/polls_app/solution/app/models/answer_choice.rb
@@ -2,5 +2,5 @@ class AnswerChoice < ActiveRecord::Base
   validates :question, :text, presence: true
 
   belongs_to :question
-  has_many :responses, inverse_of: :answer_choice
+  has_many :responses
 end
