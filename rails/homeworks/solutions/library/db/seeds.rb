ea654a428834073c1023a25632b023215f3db9e3
Quinn Leong <leongquinn@gmail.com>
2016-07-27 10:16:30 -0700

    seed rails controller project

--- rails/homeworks/solutions/library/db/seeds.rb
+++ rails/homeworks/solutions/library/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.create(title: "Harry Potter and the Order of the Phoenix", author: "J.K. Rowling")
+Book.create(title: "Lost Names", author: "Richard E. Kim")
+Book.create(title: "Extremely Loud and Incredibly Close", author: "Jonathan Safran Foer")
+Book.create(title: "Man Alive", author: "Thomas Page McBee")
+Book.create(title: "The Argonauts", author: "Maggie Nelson")
+Book.create(title: "A Little Life", author: "Hanya Yanagihara")
+Book.create(title: "The Sovereignty of Quiet", author: "Kevin Everod Quashie")
+Book.create(title: "Redefining Realness", author: "Janet Mock")
