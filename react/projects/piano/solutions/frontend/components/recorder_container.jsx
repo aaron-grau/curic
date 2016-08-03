18e82253ea59e7c0069cfb966acaa5eb977cd856
Winnie Ngo <ngowinn@gmail.com>
2016-08-02 17:52:45 -0700

    create Recorder Container

--- react/projects/piano/solutions/frontend/components/recorder_container.jsx
+++ react/projects/piano/solutions/frontend/components/recorder_container.jsx
@@ -1,3 +1,22 @@
 import { connect } from 'react-redux';
-import { startRecoding, stopRecording, addNotes } from '../actions/track_actions';
+import { startRecording, stopRecording, addNotes } from '../actions/track_actions';
 import Recorder from './recorder';
+
+const mapStateToProps = (state) => {
+  return {
+    tracks: state.tracks;
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    startRecording: timeStart => dispatch(startRecording(timeStart)),
+    stopRecording: timeNow => dispatch(startRecording(timeNow)),
+    addNotes: (timeNow, notes) => dispatch(addNotes(timeNow, notes))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Recorder);
