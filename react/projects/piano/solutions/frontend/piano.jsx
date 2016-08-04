1077043e1aec5c1897eefe48b84bdee997910a38
Winnie Ngo <ngowinn@gmail.com>
2016-08-04 13:01:57 -0700

    remove deprecated folder

--- react/projects/piano/solutions/frontend/piano.jsx
+++ react/projects/piano/solutions/frontend/piano.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import App from './components/app'; // TODO app component
-import pianoApp from './reducers'; // app reducer
+import App from './components/app';
+import pianoApp from './reducers';
 
 import PianoContainer from './components/piano_container';
 
 const store = createStore(pianoApp);
-console.log("initial state", store.getState());
 
 document.addEventListener('DOMContentLoaded', function() {
   const rootEl = document.getElementById('root');
