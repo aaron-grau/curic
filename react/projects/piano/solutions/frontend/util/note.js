ad46cf057d347da660b792a86fd1c912448bf93c
Winnie Ngo <ngowinn@gmail.com>
2016-08-05 09:53:58 -0700

    convert Note to es6 class

--- react/projects/piano/solutions/frontend/util/note.js
+++ react/projects/piano/solutions/frontend/util/note.js
@@ -16,35 +16,20 @@ const createGainNode = () => {
   return gainNode;
 };
 
-const Note = function (freq) {
-  this.oscillatorNode = createOscillator(freq);
-  this.gainNode = createGainNode();
-  this.oscillatorNode.connect(this.gainNode);
+class Note {
+  constructor(freq) {
+    this.oscillatorNode = createOscillator(freq);
+    this.gainNode = createGainNode();
+    this.oscillatorNode.connect(this.gainNode);
+  }
+
+  start() {
+    this.gainNode.gain.value = 0.3;
+  }
+
+  stop() {
+    this.gainNode.gain.value = 0;
+  }
 };
-
-Note.prototype.start = function () {
-  // can't explain the 0.3 - it's a reasonable value
-  this.gainNode.gain.value = 0.3;
-};
-
-Note.prototype.stop = function () {
-  this.gainNode.gain.value = 0;
-}
-
-// class Note {
-//   constructor(freq) {
-//     this.oscillatorNode = createOscillator(freq);
-//     this.gainNode = createGainNode();
-//     this.oscillatorNode.connect(this.gainNode);
-//   }
-//
-//   start() {
-//     this.gainNode.gain.value = 0.3;
-//   }
-//
-//   stop() {
-//     this.gainNode.gain.value = 0;
-//   }
-// };
 
 module.exports = Note;
