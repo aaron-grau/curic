42b159a79746172be53bb37d2875195c28688b9f
sewinter <samuelewinter@gmail.com>
2016-08-08 17:27:01 -0700

    classify toh_jquery

--- toh_jquery/js/bundle.js
+++ toh_jquery/js/bundle.js
@@ -60,103 +60,106 @@
 
 	/* jshint esversion: 6 */
 	
-	function View (game, $el) {
-	  this.game = game;
-	  this.$el = $el;
-	
-	  this.fromTowerIdx = null;
-	
-	  this.$el.on(
-	    "click",
-	    "ul",
-	    this.clickTower.bind(this)
-	  );
-	
-	  this.setupTowers();
-	  this.render();
-	}
-	
-	View.prototype.clickTower = function (event) {
-	  const clickedTowerIdx = $(event.currentTarget).index();
-	
-	  if (this.fromTowerIdx === null) {
-	    this.fromTowerIdx = clickedTowerIdx;
-	  } else {
-	    if (!this.game.move(this.fromTowerIdx, clickedTowerIdx)) {
-	      alert("Invalid Move! Try again.");
-	    }
+	class View {
+	  constructor(game, $el) {
+	    this.game = game;
+	    this.$el = $el;
 	
 	    this.fromTowerIdx = null;
+	
+	    this.$el.on(
+	      "click",
+	      "ul",
+	      this.clickTower.bind(this)
+	    );
+	
+	    this.setupTowers();
+	    this.render();
 	  }
 	
-	  this.render();
+	  clickTower(event) {
+	    const clickedTowerIdx = $(event.currentTarget).index();
 	
-	  if (this.game.isWon()) {
-	    // Remove click handler when done.
-	    this.$el.off("click");
-	    this.$el.addClass("game-over");
-	    alert("Good work, you!");
-	  }
-	};
+	    if (this.fromTowerIdx === null) {
+	      this.fromTowerIdx = clickedTowerIdx;
+	    } else {
+	      if (!this.game.move(this.fromTowerIdx, clickedTowerIdx)) {
+	        alert("Invalid Move! Try again.");
+	      }
 	
-	View.prototype.setupTowers = function () {
-	  /*
-	  We're setting up the skeleton for our towers
-	  here. It consist of three <ul> elements, all
-	  floated left, with each three nested <li>s.
-	  Because the <ul>s are floated, we need to
-	  add the `.group` class, containing the clearfix,
-	  to their parent. The <li> elements all will be
-	  invisible by default. Adding a disk class to
-	  them will make them visible.
-	  */
-	
-	  this.$el.empty();
-	  this.$el.addClass("group");
-	
-	  let $tower, $disk;
-	
-	  for (let towerIdx = 0; towerIdx < 3; towerIdx++) {
-	    $tower = $("<ul>");
-	
-	    for (let diskIdx = 0; diskIdx < 3; diskIdx++) {
-	      $disk = $("<li>");
-	      $tower.append($disk);
+	      this.fromTowerIdx = null;
 	    }
 	
-	    this.$el.append($tower);
-	  };
-	};
+	    this.render();
 	
-	View.prototype.render = function () {
-	  /*
-	  Rather than removing all our elements from the page
-	  and building them up again, we are removing only the
-	  classes and re-adding them as necessary. This is a
-	  more light-weight approach and will speed up the
-	  redrawing in the browser.
-	  */
-	  const $towers = this.$el.find("ul");
-	  $towers.removeClass();
-	
-	  if (this.fromTowerIdx !== null) {
-	    $towers.eq(this.fromTowerIdx).addClass("selected");
+	    if (this.game.isWon()) {
+	      // Remove click handler when done.
+	      this.$el.off("click");
+	      this.$el.addClass("game-over");
+	      alert("Good work, you!");
+	    }
 	  }
 	
-	  this.game.towers.forEach( (disks, towerIdx) => {
-	    const $disks = $towers.eq(towerIdx).children();
-	    $disks.removeClass();
+	  setupTowers() {
+	    /*
+	    We're setting up the skeleton for our towers
+	    here. It consist of three <ul> elements, all
+	    floated left, with each three nested <li>s.
+	    Because the <ul>s are floated, we need to
+	    add the `.group` class, containing the clearfix,
+	    to their parent. The <li> elements all will be
+	    invisible by default. Adding a disk class to
+	    them will make them visible.
+	    */
 	
-	    disks.forEach( (diskWidth, diskIdx) => {
-	      /*
-	      Since our disks are stacked from bottom to top
-	      as [3, 2, 1], we have to select from the back
-	      of our jQuery object, using negative indices.
-	      */
-	      $disks.eq(-1 * (diskIdx + 1)).addClass("disk-" + diskWidth);
+	    this.$el.empty();
+	    this.$el.addClass("group");
+	
+	    let $tower, $disk;
+	
+	    for (let towerIdx = 0; towerIdx < 3; towerIdx++) {
+	      $tower = $("<ul>");
+	
+	      for (let diskIdx = 0; diskIdx < 3; diskIdx++) {
+	        $disk = $("<li>");
+	        $tower.append($disk);
+	      }
+	
+	      this.$el.append($tower);
+	    };
+	  }
+	
+	  render() {
+	    /*
+	    Rather than removing all our elements from the page
+	    and building them up again, we are removing only the
+	    classes and re-adding them as necessary. This is a
+	    more light-weight approach and will speed up the
+	    redrawing in the browser.
+	    */
+	    const $towers = this.$el.find("ul");
+	    $towers.removeClass();
+	
+	    if (this.fromTowerIdx !== null) {
+	      $towers.eq(this.fromTowerIdx).addClass("selected");
+	    }
+	
+	    this.game.towers.forEach( (disks, towerIdx) => {
+	      const $disks = $towers.eq(towerIdx).children();
+	      $disks.removeClass();
+	
+	      disks.forEach( (diskWidth, diskIdx) => {
+	        /*
+	        Since our disks are stacked from bottom to top
+	        as [3, 2, 1], we have to select from the back
+	        of our jQuery object, using negative indices.
+	        */
+	        $disks.eq(-1 * (diskIdx + 1)).addClass(`disk-${diskWidth}`);
+	      });
 	    });
-	  });
-	};
+	  }
+	
+	}
 	
 	module.exports = View;
 
@@ -165,70 +168,72 @@
 /* 2 */
 /***/ function(module, exports) {
 
-	function Game () {
-	  this.towers = [[3, 2, 1], [], []];
-	};
-	
-	Game.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
-	  var startTower = this.towers[startTowerIdx];
-	  var endTower = this.towers[endTowerIdx];
-	
-	  if (startTower.length === 0) {
-	    return false;
-	  } else if (endTower.length == 0) {
-	    return true;
-	  } else {
-	    var topStartDisc = startTower[startTower.length - 1];
-	    var topEndDisc = endTower[endTower.length - 1];
-	    return topStartDisc < topEndDisc;
+	class Game {
+	  constructor() {
+	    this.towers = [[3, 2, 1], [], []];
 	  }
-	};
 	
-	Game.prototype.isWon = function () {
-	  // move all the discs to the last or second tower
-	  return (this.towers[2].length == 3) || (this.towers[1].length == 3);
-	};
+	  isValidMove(startTowerIdx, endTowerIdx) {
+	    const startTower = this.towers[startTowerIdx];
+	    const endTower = this.towers[endTowerIdx];
 	
-	Game.prototype.move = function (startTowerIdx, endTowerIdx) {
-	  if (this.isValidMove(startTowerIdx, endTowerIdx)) {
-	    this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
-	    return true;
-	  } else {
-	    return false;
-	  }
-	};
-	
-	Game.prototype.print = function () {
-	  console.log(JSON.stringify(this.towers));
-	};
-	
-	Game.prototype.promptMove = function (reader, callback) {
-	  this.print();
-	  reader.question("Enter a starting tower: ", function (start) {
-	    var startTowerIdx = parseInt(start);
-	    reader.question("Enter an ending tower: ", function (end) {
-	      var endTowerIdx = parseInt(end);
-	      callback(startTowerIdx, endTowerIdx)
-	    });
-	  });
-	};
-	
-	Game.prototype.run = function (reader, gameCompletionCallback) {
-	  this.promptMove(reader, (function (startTowerIdx, endTowerIdx) {
-	    if (!this.move(startTowerIdx, endTowerIdx)) {
-	      console.log("Invalid move!");
-	    }
-	
-	    if (!this.isWon()) {
-	      // Continue to play!
-	      this.run(reader, gameCompletionCallback);
+	    if (startTower.length === 0) {
+	      return false;
+	    } else if (endTower.length == 0) {
+	      return true;
 	    } else {
-	      this.print();
-	      console.log("You win!");
-	      gameCompletionCallback();
+	      const topStartDisc = startTower[startTower.length - 1];
+	      const topEndDisc = endTower[endTower.length - 1];
+	      return topStartDisc < topEndDisc;
 	    }
-	  }).bind(this));
-	};
+	  }
+	
+	  isWon() {
+	    // move all the discs to the last or second tower
+	    return (this.towers[2].length == 3) || (this.towers[1].length == 3);
+	  }
+	
+	  move(startTowerIdx, endTowerIdx) {
+	    if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+	      this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
+	      return true;
+	    } else {
+	      return false;
+	    }
+	  }
+	
+	  print() {
+	    console.log(JSON.stringify(this.towers));
+	  }
+	
+	  promptMove(reader, callback) {
+	    this.print();
+	    reader.question("Enter a starting tower: ", start => {
+	      const startTowerIdx = parseInt(start);
+	      reader.question("Enter an ending tower: ", end => {
+	        const endTowerIdx = parseInt(end);
+	        callback(startTowerIdx, endTowerIdx)
+	      });
+	    });
+	  }
+	
+	  run(reader, gameCompletionCallback) {
+	    this.promptMove(reader, (startTowerIdx, endTowerIdx) => {
+	      if (!this.move(startTowerIdx, endTowerIdx)) {
+	        console.log("Invalid move!");
+	      }
+	
+	      if (!this.isWon()) {
+	        // Continue to play!
+	        this.run(reader, gameCompletionCallback);
+	      } else {
+	        this.print();
+	        console.log("You win!");
+	        gameCompletionCallback();
+	      }
+	    });
+	  }
+	}
 	
 	module.exports = Game;
 
--- toh_jquery/js/hanoi-view.js
+++ toh_jquery/js/hanoi-view.js
@@ -1,101 +1,104 @@
 /* jshint esversion: 6 */
 
-function View (game, $el) {
-  this.game = game;
-  this.$el = $el;
-
-  this.fromTowerIdx = null;
-
-  this.$el.on(
-    "click",
-    "ul",
-    this.clickTower.bind(this)
-  );
-
-  this.setupTowers();
-  this.render();
-}
-
-View.prototype.clickTower = function (event) {
-  const clickedTowerIdx = $(event.currentTarget).index();
-
-  if (this.fromTowerIdx === null) {
-    this.fromTowerIdx = clickedTowerIdx;
-  } else {
-    if (!this.game.move(this.fromTowerIdx, clickedTowerIdx)) {
-      alert("Invalid Move! Try again.");
-    }
+class View {
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
 
     this.fromTowerIdx = null;
+
+    this.$el.on(
+      "click",
+      "ul",
+      this.clickTower.bind(this)
+    );
+
+    this.setupTowers();
+    this.render();
   }
 
-  this.render();
+  clickTower(event) {
+    const clickedTowerIdx = $(event.currentTarget).index();
 
-  if (this.game.isWon()) {
-    // Remove click handler when done.
-    this.$el.off("click");
-    this.$el.addClass("game-over");
-    alert("Good work, you!");
-  }
-};
+    if (this.fromTowerIdx === null) {
+      this.fromTowerIdx = clickedTowerIdx;
+    } else {
+      if (!this.game.move(this.fromTowerIdx, clickedTowerIdx)) {
+        alert("Invalid Move! Try again.");
+      }
 
-View.prototype.setupTowers = function () {
-  /*
-  We're setting up the skeleton for our towers
-  here. It consist of three <ul> elements, all
-  floated left, with each three nested <li>s.
-  Because the <ul>s are floated, we need to
-  add the `.group` class, containing the clearfix,
-  to their parent. The <li> elements all will be
-  invisible by default. Adding a disk class to
-  them will make them visible.
-  */
-
-  this.$el.empty();
-  this.$el.addClass("group");
-
-  let $tower, $disk;
-
-  for (let towerIdx = 0; towerIdx < 3; towerIdx++) {
-    $tower = $("<ul>");
-
-    for (let diskIdx = 0; diskIdx < 3; diskIdx++) {
-      $disk = $("<li>");
-      $tower.append($disk);
+      this.fromTowerIdx = null;
     }
 
-    this.$el.append($tower);
-  };
-};
+    this.render();
 
-View.prototype.render = function () {
-  /*
-  Rather than removing all our elements from the page
-  and building them up again, we are removing only the
-  classes and re-adding them as necessary. This is a
-  more light-weight approach and will speed up the
-  redrawing in the browser.
-  */
-  const $towers = this.$el.find("ul");
-  $towers.removeClass();
-
-  if (this.fromTowerIdx !== null) {
-    $towers.eq(this.fromTowerIdx).addClass("selected");
+    if (this.game.isWon()) {
+      // Remove click handler when done.
+      this.$el.off("click");
+      this.$el.addClass("game-over");
+      alert("Good work, you!");
+    }
   }
 
-  this.game.towers.forEach( (disks, towerIdx) => {
-    const $disks = $towers.eq(towerIdx).children();
-    $disks.removeClass();
+  setupTowers() {
+    /*
+    We're setting up the skeleton for our towers
+    here. It consist of three <ul> elements, all
+    floated left, with each three nested <li>s.
+    Because the <ul>s are floated, we need to
+    add the `.group` class, containing the clearfix,
+    to their parent. The <li> elements all will be
+    invisible by default. Adding a disk class to
+    them will make them visible.
+    */
 
-    disks.forEach( (diskWidth, diskIdx) => {
-      /*
-      Since our disks are stacked from bottom to top
-      as [3, 2, 1], we have to select from the back
-      of our jQuery object, using negative indices.
-      */
-      $disks.eq(-1 * (diskIdx + 1)).addClass("disk-" + diskWidth);
+    this.$el.empty();
+    this.$el.addClass("group");
+
+    let $tower, $disk;
+
+    for (let towerIdx = 0; towerIdx < 3; towerIdx++) {
+      $tower = $("<ul>");
+
+      for (let diskIdx = 0; diskIdx < 3; diskIdx++) {
+        $disk = $("<li>");
+        $tower.append($disk);
+      }
+
+      this.$el.append($tower);
+    };
+  }
+
+  render() {
+    /*
+    Rather than removing all our elements from the page
+    and building them up again, we are removing only the
+    classes and re-adding them as necessary. This is a
+    more light-weight approach and will speed up the
+    redrawing in the browser.
+    */
+    const $towers = this.$el.find("ul");
+    $towers.removeClass();
+
+    if (this.fromTowerIdx !== null) {
+      $towers.eq(this.fromTowerIdx).addClass("selected");
+    }
+
+    this.game.towers.forEach( (disks, towerIdx) => {
+      const $disks = $towers.eq(towerIdx).children();
+      $disks.removeClass();
+
+      disks.forEach( (diskWidth, diskIdx) => {
+        /*
+        Since our disks are stacked from bottom to top
+        as [3, 2, 1], we have to select from the back
+        of our jQuery object, using negative indices.
+        */
+        $disks.eq(-1 * (diskIdx + 1)).addClass(`disk-${diskWidth}`);
+      });
     });
-  });
-};
+  }
+
+}
 
 module.exports = View;
