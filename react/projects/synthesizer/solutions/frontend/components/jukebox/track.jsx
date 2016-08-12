d5afc4707dcac56ebc975f1029cf47007aa14d4a
Winnie Ngo <ngowinn@gmail.com>
2016-08-11 18:30:42 -0700

    add california girls track

--- react/projects/synthesizer/solutions/frontend/components/jukebox/track.jsx
+++ react/projects/synthesizer/solutions/frontend/components/jukebox/track.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 const Track = ({ track, disabled, onPlay, onDelete }) => (
   <div className='track' key={track.id}>
     {track.name}
-    <button
-      className='play-button'
-      disabled={disabled}
-      onClick={onPlay}>
-      Play
-    </button>
-    <button
-      className='delete-button'
-      disabled={disabled}
-      onClick={onDelete}>
-      Delete
-    </button>
+    <div className='track-buttons'>
+      <button
+        className='play-button'
+        disabled={disabled}
+        onClick={onPlay}>
+        Play
+      </button>
+      <button
+        className='delete-button'
+        disabled={disabled}
+        onClick={onDelete}>
+        Delete
+      </button>
+    </div>
   </div>
 );
 
