8ce18f27d9ca516d447d298dc2fb5164dd344e27
Ken Tran <loschorts@gmail.com>
2016-08-15 15:01:47 -0700

    delete old proj

--- react/projects/todo_react/solution/frontend/todo_react.jsx
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const TodoList = require('./components/todo_list');
-
-const root = document.getElementById('root');
-
-ReactDOM.render(<TodoList />, root);
