c7bf3336742bc7a347bec19cee07c3ae91230501
sewinter <samuelewinter@gmail.com>
2016-08-17 15:26:47 -0700

    classify mail solution; update jquery skeleton

--- javascript/projects/mail/solution/router.js
+++ javascript/projects/mail/solution/router.js
@@ -1,27 +1,29 @@
-function Router(node, routes) {
-  this.node = node;
-  this.routes = routes;
-}
+class Router {
+  constructor(node, routes) {
+    this.node = node;
+    this.routes = routes;
+  }
 
-Router.prototype.start = function () {
-  this.render();
-  window.addEventListener("hashchange", () => {
+  start() {
     this.render();
-  });
-};
+    window.addEventListener("hashchange", () => {
+      this.render();
+    });
+  }
 
-Router.prototype.render = function () {
-  this.node.innerHTML = "";
-  let component = this.activeRoute();
-  if(component) {
-    this.node.appendChild(component.render());
+  render() {
+    this.node.innerHTML = "";
+    let component = this.activeRoute();
+    if(component) {
+      this.node.appendChild(component.render());
+    }
+  }
+
+  activeRoute() {
+    let hash = window.location.hash.substr(1);
+    let component = this.routes[hash];
+    return component;
   }
 }
-
-Router.prototype.activeRoute = function () {
-  let hash = window.location.hash.substr(1);
-  let component = this.routes[hash];
-  return component;
-};
 
 module.exports = Router;
