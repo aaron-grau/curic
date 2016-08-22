e2f287815bdcd57effc6eb546a78ecebfdff17c9
Ken Tran <loschorts@gmail.com>
2016-08-22 10:29:49 -0700

    calculator formatting: remove extraneous props call and use object shorthand

--- react/homeworks/solutions/calculator/calculator.jsx
+++ react/homeworks/solutions/calculator/calculator.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 class Calculator extends React.Component{
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = { num1: "", num2: "", result: 0 };
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
@@ -15,36 +15,36 @@ class Calculator extends React.Component{
 
   setNum1(e){
     const num1 = e.target.value ? parseInt(e.target.value) : "";
-    this.setState({ num1: num1 });
+    this.setState({ num1 });
   }
 
   setNum2(e){
     const num2 = e.target.value ? parseInt(e.target.value) : "";
-    this.setState({ num2: num2 });
+    this.setState({ num2 });
   }
 
   add(e){
     e.preventDefault();
     const result = this.state.num1 + this.state.num2;
-    this.setState({ result: result });
+    this.setState({ result });
   }
 
   subtract(e){
     e.preventDefault();
     const result = this.state.num1 - this.state.num2;
-    this.setState({ result: result });
+    this.setState({ result });
   }
 
   multiply(e){
     e.preventDefault();
     const result = this.state.num1 * this.state.num2;
-    this.setState({ result: result });
+    this.setState({ result });
   }
 
   divide(e){
     e.preventDefault();
     const result = this.state.num1 / this.state.num2;
-    this.setState({ result: result });
+    this.setState({ result });
   }
 
   clear(e){
@@ -62,7 +62,6 @@ class Calculator extends React.Component{
         <input onChange={this.setNum2} value={num2}/>
         <button onClick={this.clear}>Clear</button>
         <br />
-
         <button onClick={this.add}>+</button>
         <button onClick={this.subtract}>-</button>
         <button onClick={this.multiply}>*</button>
