f9a8730d8e7ec0d8f484ec76b307807c9045fd0c
Rage Gage <ragegage@Rages-MacBook-Air.local>
2016-09-15 15:01:58 -0700

    removes action constants object

--- react/projects/pokedex/solution/frontend/reducers/pokemon_reducer.js
+++ react/projects/pokedex/solution/frontend/reducers/pokemon_reducer.js
@@ -1,24 +1,25 @@
-import { PokemonConstants } from '../actions/pokemon_actions';
+import { RECEIVE_ALL_POKEMON, RECEIVE_SINGLE_POKEMON, RECEIVE_NEW_POKEMON, 
+  POKEMON_ERROR, REQUEST_ALL_POKEMON, REQUEST_SINGLE_POKEMON } from '../actions/pokemon_actions';
 import merge from 'lodash/merge';
 
 const PokemonReducer = (oldState = {loading: false}, action) => {
   switch (action.type) {
-    case PokemonConstants.RECEIVE_ALL_POKEMON:
+    case RECEIVE_ALL_POKEMON:
       oldState.loading = false;
       return merge({}, oldState, {pokemon: action.pokemon});
-    case PokemonConstants.RECEIVE_SINGLE_POKEMON:
+    case RECEIVE_SINGLE_POKEMON:
       oldState.loading = false;
       return merge({}, oldState, {pokemonDetail: action.pokemon});
-    case PokemonConstants.RECEIVE_NEW_POKEMON:
+    case RECEIVE_NEW_POKEMON:
       action.pokemon.toys = {};
       return merge({}, oldState, {
         pokemonDetail: action.pokemon, pokemon: [...oldState.pokemon, action.pokemon]
       });
-    case PokemonConstants.POKEMON_ERROR:
+    case POKEMON_ERROR:
       return merge({}, oldState, {pokemonErrors: action.errors});
-    case PokemonConstants.REQUEST_ALL_POKEMON:
+    case REQUEST_ALL_POKEMON:
       return merge({}, oldState, {loading: true});
-    case PokemonConstants.REQUEST_SINGLE_POKEMON:
+    case REQUEST_SINGLE_POKEMON:
       return merge({}, oldState, {loading: true});
     default:
       return oldState;
