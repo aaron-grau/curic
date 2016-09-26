f67e544ac43cea52f964aa75d6a2450a2bb953cd
Rage Gage <ragegage@Rages-MacBook-Air.local>
2016-09-26 16:35:49 -0700

    update root component to use pokedex onEnter pattern

--- react/projects/bench_bnb/solution/frontend/components/root.jsx
+++ react/projects/bench_bnb/solution/frontend/components/root.jsx
@@ -10,34 +10,39 @@ import BenchShowContainer from './bench_show/bench_show_container';
 import ReviewFormContainer from './bench_show/review_form_container';
 import SessionFormContainer from './session_form/session_form_container';
 
-function ensureLoggedIn({ getState }, nextState, replace){
-  const currentUser = getState().session.currentUser;
-  if (!currentUser) {
-    replace('/login');
-  }
-}
 
-function redirectIfLoggedIn({ getState }, nextState, replace){
-  const currentUser = getState().session.currentUser;
-  if (currentUser) {
-    replace('/');
-  }
-}
 
-const Root = ({store}) => (
-  <Provider store={store}>
-    <Router history={ hashHistory }>
-      <Route path="/" component={ App }>
-        <IndexRoute component={ SearchContainer } />
-        <Route path="/login" component={ SessionFormContainer } onEnter={redirectIfLoggedIn.bind(null, store)}/>
-        <Route path="/signup" component={ SessionFormContainer } onEnter={redirectIfLoggedIn.bind(null, store)}/>
-        <Route path="/benches/new" component={ BenchFormContainer } onEnter={ ensureLoggedIn.bind(null, store) }/>
-        <Route path="/benches/:benchId" component={ BenchShowContainer} >
-          <Route path="review" component={ ReviewFormContainer } onEnter={ ensureLoggedIn.bind(null, store) }/>
+const Root = ({store}) => {
+
+  const ensureLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (!currentUser) {
+      replace('/login');
+    }
+  };
+
+  const redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  }
+
+  return (
+    <Provider store={store}>
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App }>
+          <IndexRoute component={ SearchContainer } />
+          <Route path="/login" component={ SessionFormContainer } onEnter={redirectIfLoggedIn.bind(null, store)}/>
+          <Route path="/signup" component={ SessionFormContainer } onEnter={redirectIfLoggedIn.bind(null, store)}/>
+          <Route path="/benches/new" component={ BenchFormContainer } onEnter={ ensureLoggedIn.bind}/>
+          <Route path="/benches/:benchId" component={ BenchShowContainer} >
+            <Route path="review" component={ ReviewFormContainer } onEnter={ ensureLoggedIn.bind}/>
+          </Route>
         </Route>
-      </Route>
-    </Router>
-  </Provider>
-);
+      </Router>
+    </Provider>
+  );
+};
 
 export default Root;
