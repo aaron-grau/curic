0cdb8de355bc7b6de140e2dfc5d8fb6d211df42a
Ken Tran <loschorts@gmail.com>
2016-09-27 16:04:59 -0700

    add four_sum solution

--- ruby/projects/two_sum/solution/two_sum.rb
+++ ruby/projects/two_sum/solution/two_sum.rb
@@ -69,15 +69,65 @@ def two_sum_indices(arr, target_sum)
     nil
 end
 
-def four_sum? list, target
-  (0...list.length).each do |x|
-    (x+1...list.length).each do |y|
-      pair_sum = list[x] + list[y]
-      remainder = list.dup
-      remainder.delete_at(remainder.index(list[x]))
-      remainder.delete_at(remainder.index(list[y]))
-      return true if two_sum?(remainder, target - pair_sum)
+# O(n^2) time complexity
+# O(n^2) space complexity
+
+def four_sum?(arr, target_sum) 
+
+  pairs = Hash.new
+
+  # pairs: keys are sums, values are hashes of composites pairs[sum]: keys are
+  # composite values, values are sets of locations where values appear
+
+  arr.each_with_index do |a, first|
+    arr.each_with_index do |b, second|
+
+      next unless first < second
+
+      sum = [a + b].reduce(:+)
+
+      # initializes the data structures if not already done
+      pairs[sum] ||= Hash.new
+      pairs[sum][a] ||= Set.new
+      pairs[sum][b] ||= Set.new
+
+      # stores the indices that contain `a` and `b` values, the first two numbers of a possible foursome
+      pairs[sum][a] << first
+      pairs[sum][b] << second
+
+      # finds the complement required to complete the sum
+      complement = target_sum - sum
+
+      # moves on unless there is a pair that sums to the complement-sum
+      next unless pairs[complement]
+
+      # iterates over the complement-pairs, looking for possible values `c` that can be the third number in the foursome 
+
+      pairs[complement].each do |c, thirds|
+        # skip if no unused indices of the required value `c` remain
+        next if (thirds - [first, second]).empty?
+
+        # find the last required value `d` to complete the foursome
+        d = complement - c
+
+        # iterate over the thirds to find a fourth that is distinct from first, second, and third
+        (thirds - [first, second]).each do |third|
+          return true unless (pairs[complement][d] - [first, second, third]).empty?
+        end
+
+      end
+
+      # The loops above run whenever there is a possible complementary pair. We
+      # need to confirm that there is no overlap between the original pair of
+      # `a`, `b` and the complementary pair `c`, `d`. Using sets lets us exclude
+      # `a`, `b`, and `c` in turn from being used as `d` in constant time.
+      # Because these checks happen as the pairs as forming, we stop checking as
+      # soon as we find a match, avoiding having to look at all the permutations
+      # in the worst case scenario of a homogenous array of target_sum/4 values.
+
     end
   end
+
   false
+
 end
