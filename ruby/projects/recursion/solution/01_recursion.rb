9ce3e6056d2d896031ab599c87af62fe560fbb6d
munyofrey <munyofrey@gmail.com>
2016-09-28 17:08:23 -0700

    added comments to solution and rezipped

--- ruby/projects/recursion/solution/01_recursion.rb
+++ ruby/projects/recursion/solution/01_recursion.rb
@@ -80,16 +80,29 @@ end
 def permutations(array)
   return [array] if array.length <= 1
 
-  first = array.shift
-  perms = permutations(array)
-  total_perms = []
 
+  # Similar to the subsets problem, we observe that to get the permutations
+  # of [1, 2, 3] we can look at the permutations of [1, 2] which are
+  # [1, 2] and [2, 1] and add the last element to every possible index getting
+  # [3, 1, 2], [1, 3, 2], [1, 2, 3], [3, 2, 1], [2, 3, 1]
+
+  # pop off the last element
+  first = array.shift
+  # make the recursive call
+  perms = permutations(array)
+  # we will need an array to store all our different permutations
+  total_permutations = []
+
+
+  # Now we iterate over the result of our recusive call say [[1, 2], [2, 1]]
+  # and for each permutation add first into every index. This new subarray
+  # gets added to total_permutations.
   perms.each do |perm|
     (0..perm.length).each do |i|
-      total_perms << perm[0 ... i] + [first] + perm[i .. -1]
+      total_permutations << perm[0 ... i] + [first] + perm[i .. -1]
     end
   end
-  total_perms
+  total_permutations
 end
 
 def bsearch(nums, target)
