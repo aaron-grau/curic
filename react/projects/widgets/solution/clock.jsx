7f2bad81c1bf67ae3c159e0214d74f044c36db42
Shamayel Daoud <amaroqkapuzing@gmail.com>
2016-10-12 14:30:02 -0700

    add CSS steps

--- react/projects/widgets/solution/clock.jsx
+++ react/projects/widgets/solution/clock.jsx
@@ -14,6 +14,10 @@ export default class Clock extends React.Component {
     this.intervalId = setInterval(this.tick, 1000);
   }
 
+  componentWillUnmount() {
+    this.intervalId.clearInterval();
+  }
+
   tick() {
     this.setState({time: new Date()});
   }
