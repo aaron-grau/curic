5ff5d1837afaaa52a1107ecd5c7391c2563ff71e
Ryan <Hall.Ryan.R@gmail.com>
2016-10-12 17:11:56 -0700

    undo css

--- react/projects/pokedex/skeleton/frontend/reducers/selectors.js
+++
@@ -1,7 +0,0 @@
-export const selectPokemonToy = ({pokemonDetail}, itemId) => pokemonDetail.items[itemId] || {};
-
-export const selectItems = ({pokemonDetail}) => {
-	return pokemonDetail && pokemonDetail.items ? Object.keys(pokemonDetail.items).map((itemId) => {
-		return pokemonDetail.items[itemId];
-	}) : [];
-};
