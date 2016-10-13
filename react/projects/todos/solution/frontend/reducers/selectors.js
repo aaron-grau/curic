dbc8bb4dacf79d08117b06d9fe0875af37cacd33
Jennifer Georgevich <jenngeorge@Jennifers-MacBook-Air.local>
2016-10-13 11:30:14 -0700

    update selector style: remove explicit return thx Robert

--- react/projects/todos/solution/frontend/reducers/selectors.js
+++ react/projects/todos/solution/frontend/reducers/selectors.js
@@ -1,6 +1,6 @@
 
 export const allTodos = ({ todos }) => Object.keys(todos).map(id => todos[id]);
 
-export const stepsById = ({ steps }, todo_id) => {
-  return steps[todo_id] ? Object.keys(steps[todo_id]).map(key => steps[todo_id][key]) : [];
-};
+export const stepsById = ({ steps }, todo_id) => (
+  steps[todo_id] ? Object.keys(steps[todo_id]).map(key => steps[todo_id][key]) : []
+);
