192027310598686d16e7254578bb79983c475a76
Ryan <Hall.Ryan.R@gmail.com>
2016-10-12 17:12:57 -0700

    undo views and controllers

--- react/projects/pokedex/skeleton/config/routes.rb
+++ react/projects/pokedex/skeleton/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
 
-  namespace :api, defaults: {format: :json} do
-    resources :pokemon, only: [:create, :destroy, :index, :show, :update]
-    resources :items, only: [:show, :update]
-  end
 end
