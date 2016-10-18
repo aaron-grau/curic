c413797802defdad8f30af14bc13e0ed9aaab855
Ryan <Hall.Ryan.R@gmail.com>
2016-10-18 12:19:18 -0700

    with router in item.jsx

--- react/projects/pokedex/solution/frontend/components/items/item.jsx
+++ react/projects/pokedex/solution/frontend/components/items/item.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import History from '../../util/history';
+import { withRouter } from 'react-router';
 
-const handleClick = url => (
-  () => History.push(url)
-);
+const ToyItem = ({item, router}) => {
+  const handleClick = url => () => router.push(url);
 
-const ToyItem = ({item}) => (
+  return(
 		<li onClick={handleClick(`/pokemon/${item.pokemon_id}/item/${item.id}`)}>
 			<img src={item.image_url} alt={item.name}/>
 		</li>
-);
+  )
+};
 
 
-export default ToyItem;
+export default withRouter(ToyItem);
