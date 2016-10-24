32b4d77fafcd1e07e892ff6edadf513038149b5f
munyofrey <munyofrey@gmail.com>
2016-10-24 11:08:28 -0700

    update solution to remove unnecessary object

--- react/projects/synthesizer/solution/frontend/store/store.js
+++ react/projects/synthesizer/solution/frontend/store/store.js
@@ -2,14 +2,7 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
 
-const preloadedState = {
-  notes: [],
-  tracks: {},
-  isRecording: false,
-  isPlaying: false
- }
-
-const configureStore = (preloadedState) => (
+const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState
