dd14b28536983e3ad9708db690fa46028dd07b35
meagan <mfeniseycopes@gmail.com>
2016-11-02 16:09:52 -0400

    Fix hardcoded seed files and rezip

--- sql/projects/associations_exercise/skeleton/db/seeds.rb
+++ sql/projects/associations_exercise/skeleton/db/seeds.rb
@@ -1,17 +1,20 @@
 ActiveRecord::Base.transaction do
 
-  User.create(:name => "CJ")
-  User.create(:name => "Flarnie")
-  User.create(:name => "Jeff")
-  User.create(:name => "Georges St. Pierre")
-  User.create(:name => "Ned")
+  User.destroy_all
+  user1 = User.create(:name => "CJ")
+  user2 = User.create(:name => "Flarnie")
+  user3 = User.create(:name => "Jeff")
+  user4 = User.create(:name => "Georges St. Pierre")
+  user5 = User.create(:name => "Ned")
 
-  Course.create(:name => "Ruby 101", :instructor_id => 5)
-  Course.create(:name => "Ruby 102", :instructor_id => 5, prereq_id: 1)
+  Course.destroy_all
+  course1 = Course.create(:name => "Ruby 101", :instructor_id => user5.id)
+  course2 = Course.create(:name => "Ruby 102", :instructor_id => user5.id, prereq_id: course1.id)
 
-  Enrollment.create(:student_id => 3, :course_id => 1)
-  Enrollment.create(:student_id => 4, :course_id => 1)
-  Enrollment.create(:student_id => 1, :course_id => 2)
-  Enrollment.create(:student_id => 2, :course_id => 2)
+  Enrollment.destroy_all
+  Enrollment.create(:student_id => user3.id, :course_id => course1.id)
+  Enrollment.create(:student_id => user4.id, :course_id => course1.id)
+  Enrollment.create(:student_id => user1.id, :course_id => course2.id)
+  Enrollment.create(:student_id => user2.id, :course_id => course2.id)
 
 end
