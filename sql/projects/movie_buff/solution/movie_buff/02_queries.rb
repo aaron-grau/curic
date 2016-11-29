368d97829cdf0cfbb36b844cedd201367def7448
Robert Koeze <rkoeze@gmail.com>
2016-11-28 21:15:20 -0800

    remove select from query using pluck

--- sql/projects/movie_buff/solution/movie_buff/02_queries.rb
+++ sql/projects/movie_buff/solution/movie_buff/02_queries.rb
@@ -8,7 +8,7 @@ end
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
   
-  Movie.select(:yr).group(:yr).having("MAX(score) < 8").pluck(:yr)
+  Movie.group(:yr).having("MAX(score) < 8").pluck(:yr)
 end
 
 def cast_list(title)
