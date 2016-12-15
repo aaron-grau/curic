f07969d20af340d99471a755ae2318c654217abb
Luke Wassink <lwassink@gmail.com>
2016-12-15 11:38:16 -0800

    remove redux dev-tools

--- react/projects/bench_bnb/solution/frontend/store/store.js
+++ react/projects/bench_bnb/solution/frontend/store/store.js
@@ -1,15 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import { compose } from 'redux';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(thunk))
+    applyMiddleware(thunk)
   )
 )
 
