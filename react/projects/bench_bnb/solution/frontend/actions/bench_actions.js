4fb97cf454bbbdb10c45cf44587c0dff9f9da90f
Luke Wassink <lwassink@gmail.com>
2016-12-15 10:15:08 -0800

    add reviews to state on creation

--- react/projects/bench_bnb/solution/frontend/actions/bench_actions.js
+++ react/projects/bench_bnb/solution/frontend/actions/bench_actions.js
@@ -2,6 +2,12 @@ import * as APIUtil from '../util/bench_api_util'
 
 export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
 export const RECEIVE_BENCH = "RECEIVE_BENCH";
+export const RECEIVE_REVIEW = "RECEIVE_REVIEW";
+
+export const createReview = review => dispatch => (
+  APIUtil.createReview(review)
+    .then(review => dispatch(receiveReview(review)))
+);
 
 export function fetchBenches(filters) {
   return (dispatch) => {
@@ -32,5 +38,10 @@ export const receiveBenches = benches => ({
 export const receiveBench = bench => ({
   type: RECEIVE_BENCH,
   bench
+});
+
+export const receiveReview = review => ({
+  type: RECEIVE_REVIEW,
+  review
 });
 
