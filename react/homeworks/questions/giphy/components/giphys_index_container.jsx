02a6f0e41fc396e9fb3375e8c7bc3b80f556388b
Quinn Leong <leongquinn@gmail.com>
2016-12-19 17:11:33 -0800

    skeleton out new giphy hw

---
+++ react/homeworks/questions/giphy/components/giphys_index_container.jsx
@@ -0,0 +1,3 @@
+import { connect } from 'react-redux';
+import GiphysSearchBar from './giphys_search_bar';
+import { fetchSearchGiphys } from '../actions/giphy_actions';
---
+++ react/homeworks/questions/giphy/components/root.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import GiphysIndexContainer from './giphys_index_container';
+import GiphysSearchBar from './giphys_search_bar';
