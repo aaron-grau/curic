9bf13211176d02967983e7a94ac6a9ae9921b802
Daniel Colson <danieljamescolson@gmail.com>
2017-02-09 14:30:14 -0500

    Remove unused demos

--- react/demos/recipes_demo/pre_flux/frontend/entry.jsx
+++
@@ -1,9 +0,0 @@
-const ReactDOM = require('react-dom');
-const React = require('react');
-const Recipes = require('./components/recipes');
-
-document.addEventListener("DOMContentLoaded", function () {
-  const root = document.querySelector('#root');
-  ReactDOM.render(<Recipes />, root);
-});
-
