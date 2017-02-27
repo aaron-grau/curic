05fec51f19465ea8e342a5afb81423da8615fedf
Robert Koeze <rkoeze@gmail.com>
2017-02-27 11:51:12 -0800

    format comments table

--- rails/projects/art_share_api/solution/db/migrate/20170227191050_create_comments.rb
+++ rails/projects/art_share_api/solution/db/migrate/20170227191050_create_comments.rb
@@ -1,9 +1,10 @@
-class Comments < ActiveRecord::Migration
+class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body, null: false
       t.integer :user_id, null: false
       t.integer :artwork_id, null: false
+
       t.timestamps null: false
     end
     
