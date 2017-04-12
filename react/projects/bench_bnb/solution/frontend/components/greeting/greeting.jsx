3fd2e366fc37f0132176e5b2696cb12ea45ff3d8
Robert Koeze <rkoeze@gmail.com>
2017-04-12 15:50:16 -0700

    remove unused activeClass prop

--- react/projects/bench_bnb/solution/frontend/components/greeting/greeting.jsx
+++ react/projects/bench_bnb/solution/frontend/components/greeting/greeting.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const sessionLinks = () => (
   <nav className="login-signup">
-    <Link to="/login" activeClassName="current">Login</Link>
+    <Link to="/login">Login</Link>
     &nbsp;or&nbsp;
-    <Link to="/signup" activeClassName="current">Sign up!</Link>
+    <Link to="/signup">Sign up!</Link>
   </nav>
 );
 
