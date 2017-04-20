29a13bb122b5774bec2531a96dc9de500b1b2fc1
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-04-20 12:19:46 -0400

    Initialize new error handling project

---
+++ ruby/projects/a_user_friendly_library/README.md
@@ -0,0 +1,7 @@
+# A User Friendly Library
+
+oh noes, we made this "super useful" library, but it keeps throwing fugly error
+messages that are hard to understand. Let's revamp the library to throw more
+descriptive errors and prevent wrongful use.
+
+
---
+++ ruby/projects/a_user_friendly_library/super_useful.rb
@@ -0,0 +1,40 @@
+def convert_to_num(str)
+  str.to_i
+end
+
+FRUITS = ["apple", "banana", "orange"]
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  maybe_fruit = gets
+  
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  else
+    raise Exception 
+  end 
+end  
+
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}ing. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
