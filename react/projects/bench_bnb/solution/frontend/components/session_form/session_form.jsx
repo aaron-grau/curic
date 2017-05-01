968e9ea083360fd3a4e611433b6af1a0593436cf
Robert Koeze <rkoeze@gmail.com>
2017-05-01 14:40:41 -0700

    add space to state object

--- react/projects/bench_bnb/solution/frontend/components/session_form/session_form.jsx
+++ react/projects/bench_bnb/solution/frontend/components/session_form/session_form.jsx
@@ -4,7 +4,7 @@ import { Link, withRouter } from 'react-router-dom';
 class SessionForm extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { username: "", password: ""};
+		this.state = { username: "", password: "" };
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
