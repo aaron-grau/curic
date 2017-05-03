97ca6fe11a4e9c5c9f8d9c93efef98fd81a77d21
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-05-03 13:44:01 -0400

    Reorganize ghost helper methods (they cray)

--- ruby/projects/ghost/solution/player.rb
+++ ruby/projects/ghost/solution/player.rb
@@ -8,8 +8,7 @@ class Player
   def alert_invalid_move(letter)
     puts "#{letter} is not a valid move!"
     puts "Your guess must be a letter of the alphabet."
-    puts "You must be able to form a word starting with the new fragment."
-    puts
+    puts "You must be able to form a word starting with the new fragment.\n"
   end
 
   def guess(fragment)
