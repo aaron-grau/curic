fddd6f30b1fa250a45c7782d0151af736881ee33
Luke Wassink <lwassink@gmail.com>
2017-05-10 16:39:24 -0700

    import Link

--- react/homeworks/rainbow_routes/skeleton/components/red.jsx
+++ react/homeworks/rainbow_routes/skeleton/components/red.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, Link, NavLink } from 'react-router-dom';
 import Orange from './orange';
 import Yellow from './yellow';
 
