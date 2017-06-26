96ce0dc21ac4c1f8cb5d91cf11dd90ff4d905418
louiscruz <louisstephancruz@me.com>
2017-06-26 09:30:56 -0700

    Fix tmp in gitignore and update before_filters

--- rails/projects/goal_app/solution/app/controllers/users_controller.rb
+++ rails/projects/goal_app/solution/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :require_no_current_user!, :only => [:new, :create]
+  before_action :require_no_current_user!, only: [:new, :create]
 
   def new
     @user = User.new
