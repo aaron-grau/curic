30c527735e77bd8ca05ab46c9ae28d887aca9564
David Dobrynin <david.dobrynin@gmail.com>
2017-07-08 00:58:14 -0700

    Add premium column to users table

--- sql/projects/url_shortener/db/migrate/20170708074812_create_users.rb
+++ sql/projects/url_shortener/db/migrate/20170708074812_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :email, null: false
-
+      t.boolean :premium, default: false
       t.timestamps
     end
     add_index :users, :email, unique: true
