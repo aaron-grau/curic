d34339d12b28d03b5fc90368034dc3d9a3fb187e
louiscruz <louisstephancruz@me.com>
2017-07-11 13:08:31 -0700

    Update n+1 buster

--- sql/homeworks/n_1_buster/solution/app/models/driver.rb
+++ sql/homeworks/n_1_buster/solution/app/models/driver.rb
@@ -1,8 +1,6 @@
-class Driver < ActiveRecord::Base
-  belongs_to(
-    :bus,
-    class_name: "Bus",
+class Driver < ApplicationRecord
+  belongs_to :bus,
+    class_name: 'Bus',
     foreign_key: :bus_id,
     primary_key: :id
-  )
 end
