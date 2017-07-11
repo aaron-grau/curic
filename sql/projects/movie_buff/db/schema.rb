2ba0d3d07fa65614d8d52ea3d0a2c19e2141886d
Aaron Wayne <amwayne90@gmail.com>
2017-07-11 10:50:20 -0700

    Add solutions for movie_buff

--- sql/projects/movie_buff/db/schema.rb
+++ sql/projects/movie_buff/db/schema.rb
@@ -10,16 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707001210) do
+ActiveRecord::Schema.define(version: 20170711160807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "actors", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_actors_on_name"
+  end
+
+  create_table "castings", force: :cascade do |t|
+    t.integer "actor_id", null: false
+    t.integer "movie_id", null: false
+    t.integer "ord", null: false
+    t.index ["actor_id", "movie_id"], name: "index_castings_on_actor_id_and_movie_id", unique: true
+    t.index ["actor_id"], name: "index_castings_on_actor_id"
+    t.index ["movie_id"], name: "index_castings_on_movie_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "yr", null: false
+    t.float "score", null: false
+    t.integer "votes", null: false
+    t.integer "director_id", null: false
+    t.index ["director_id"], name: "index_movies_on_director_id"
+    t.index ["title"], name: "index_movies_on_title"
   end
 
 end
