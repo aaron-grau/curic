c9aeb292f99d3ade8e2602531507ddf223a09955
David Dobrynin <david.dobrynin@gmail.com>
2017-07-11 20:36:46 -0700

    Create Tagging model

--- sql/projects/url_shortener/app/models/tagging.rb
+++ sql/projects/url_shortener/app/models/tagging.rb
@@ -10,4 +10,17 @@
 #
 
 class Tagging < ApplicationRecord
+  validates :shortened_url, :tag_topic, presence: true
+  validates :shortened_url_id, uniqueness: { scope: :tag_topic_id }
+
+  belongs_to :tag_topic,
+  primary_key: :id,
+  foreign_key: :tag_topic_id,
+  class_name: :TagTopic
+
+
+  belongs_to :shortened_url,
+  primary_key: :id,
+  foreign_key: :shortened_url_id,
+  class_name: :ShortenedUrl
 end
