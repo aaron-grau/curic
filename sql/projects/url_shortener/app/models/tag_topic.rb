84b16911ebcc6c91bcbff97faa04598c7b6de863
David Dobrynin <david.dobrynin@gmail.com>
2017-07-11 20:33:52 -0700

    Create TagTopic model

--- sql/projects/url_shortener/app/models/tag_topic.rb
+++ sql/projects/url_shortener/app/models/tag_topic.rb
@@ -9,4 +9,24 @@
 #
 
 class TagTopic < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :taggings,
+  primary_key: :id,
+  foreign_key: :tag_topic_id,
+  class_name: :Tagging,
+  dependent: :destroy
+
+  has_many :shortened_urls,
+  through: :taggings,
+  source: :shortened_url
+
+
+  def popular_links
+    shortened_urls.joins(:visits)
+      .group(:short_url)
+      .order('COUNT(visits.id) DESC')
+      .select('long_url, short_url, COUNT(visits.id) as number_of_visits')
+      .limit(5)
+  end
 end
