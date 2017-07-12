ca86029f6e7608b2401c7bc5f8ed3079407ea9c5
David Dobrynin <david.dobrynin@gmail.com>
2017-07-11 20:41:58 -0700

    Create Visit model

--- sql/projects/url_shortener/app/models/visit.rb
+++ sql/projects/url_shortener/app/models/visit.rb
@@ -10,4 +10,19 @@
 #
 
 class Visit < ApplicationRecord
+  validates :visitor, :shortened_url, presence: true
+
+  # This is short hand syntax. Since the primary key, foreign key, and
+  # class name all follow convention, Rails can guess their values
+  # without you having to explicitly state them.
+  belongs_to :shortened_url
+
+  belongs_to :visitor,
+  class_name: :User,
+  foreign_key: :user_id,
+  primary_key: :id
+
+  def self.record_visit!(user, shortened_url)
+    Visit.create!(user_id: user.id, shortened_url_id: shortened_url.id)
+  end
 end
