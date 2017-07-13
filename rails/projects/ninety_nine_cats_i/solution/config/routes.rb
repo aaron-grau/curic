4c4c8b1be6a5d2e6c1be985b116d44f0d54ba680
David Dobrynin <david.dobrynin@gmail.com>
2017-07-12 22:27:50 -0700

    Initial commit

--- rails/projects/ninety_nine_cats_i/solution/config/routes.rb
+++ rails/projects/ninety_nine_cats_i/solution/config/routes.rb
@@ -1,9 +1,3 @@
-NinetyNineCatsDay1::Application.routes.draw do
-  resources :cats, except: :destroy
-  resources :cat_rental_requests, only: [:create, :new] do
-    post "approve", on: :member
-    post "deny", on: :member
-  end
-
-  root to: redirect("/cats")
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
