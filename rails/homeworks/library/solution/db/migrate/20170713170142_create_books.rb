e9434444425b516abbf050ee9134329afd72c01f
louiscruz <louisstephancruz@me.com>
2017-07-13 10:13:57 -0700

    Update library

--- rails/homeworks/library/solution/db/migrate/20170713170142_create_books.rb
+++ rails/homeworks/library/solution/db/migrate/20170713170142_create_books.rb
@@ -1,10 +1,10 @@
-class CreateBooks < ActiveRecord::Migration
+class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
       t.string :title, null: false
       t.string :author, null: false
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
