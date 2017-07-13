ad6f3512e2f6e4dc138262643a89fff5a7d500ce
David Dobrynin <david.dobrynin@gmail.com>
2017-07-12 22:48:01 -0700

    Create Cat model

--- rails/projects/ninety_nine_cats_i/solution/app/models/cat.rb
+++ rails/projects/ninety_nine_cats_i/solution/app/models/cat.rb
@@ -1,2 +1,33 @@
+require 'action_view'
+
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  # .freeze renders a constant immutable.
+  CAT_COLORS = %w(black white orange brown).freeze
+
+  # Remember, has_many is just a method where the first argument is
+  # the name of the association, and the second argument is an options
+  # hash.
+
+  has_many(
+    :rental_requests,
+    class_name: :CatRentalRequest,
+    dependent: :destroy
+  )
+
+  validates(
+    :birth_date,
+    :color,
+    :name,
+    :sex,
+    presence: true
+  )
+
+  validates :color, inclusion: CAT_COLORS
+  validates :sex, inclusion: %w(M F)
+
+  def age
+    time_ago_in_words(birth_date)
+  end
 end
