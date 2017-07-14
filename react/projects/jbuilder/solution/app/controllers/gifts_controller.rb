faf743e882a1c8c28b2cb9615123fdb5a509dc83
David Dobrynin <david.dobrynin@gmail.com>
2017-07-14 14:55:19 -0700

    Create controllers and views

---
+++ react/projects/jbuilder/solution/app/controllers/gifts_controller.rb
@@ -0,0 +1,7 @@
+class GiftsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- react/projects/jbuilder/solution/config/routes.rb
+++ react/projects/jbuilder/solution/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'parties/index'
+
+  get 'parties/show'
+
+  get 'guests/index'
+
+  get 'guests/show'
+
+  get 'gifts/index'
+
+  get 'gifts/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
