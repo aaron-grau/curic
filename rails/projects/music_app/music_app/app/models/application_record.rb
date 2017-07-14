6c262b7a06176135b455b107f3c5fa707a28b2d0
Aaron Wayne <amwayne90@gmail.com>
2017-07-13 22:53:46 -0700

    Small syntax changes

--- rails/projects/music_app/music_app/app/models/application_record.rb
+++ rails/projects/music_app/music_app/app/models/application_record.rb
@@ -2,10 +2,10 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def generate_unique_token_for_field(field)
-    token = SecureRandom.base64(16)
+    token = SecureRandom.base64_urlsafe(16)
 
     while self.class.exists?(field => token)
-      token = SecureRandom.base64(16)
+      token = SecureRandom.base64_urlsafe(16)
     end
 
     token
