b339fc68e79a3bb9daa34cccf12a7184b7dcb563
David Dobrynin <david.dobrynin@gmail.com>
2017-07-13 22:55:27 -0700

    Delete old folders

--- rails/projects/ninety_nine_cats_i_old/solution/config/routes.rb
+++
@@ -1,9 +0,0 @@
-NinetyNineCatsDay1::Application.routes.draw do
-  resources :cats, except: :destroy
-  resources :cat_rental_requests, only: [:create, :new] do
-    post "approve", on: :member
-    post "deny", on: :member
-  end
-
-  root to: redirect("/cats")
-end
