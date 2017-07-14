0a66dbef63ebf0ae82638e89d4fbbfcdb3dd7d31
Aaron Wayne <amwayne90@gmail.com>
2017-07-13 17:50:39 -0700

    Finish controllers

--- rails/projects/music_app/music_app/app/controllers/albums_controller.rb
+++ rails/projects/music_app/music_app/app/controllers/albums_controller.rb
@@ -1,19 +1,54 @@
 class AlbumsController < ApplicationController
-  def show
-  end
-
-  def new
-  end
+  before_action :require_user!
 
   def create
-  end
+    @album = Album.new(album_params)
 
-  def edit
-  end
-
-  def update
+    if @album.save
+      redirect_to album_url(@album)
+    else
+      @band = @album.band
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to band_url(@album.band_id)
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+    render :edit
+  end
+
+  def new
+    @band = Band.find(params[:band_id])
+    @album = Album.new(band_id: params[:band_id])
+    render :new
+  end
+
+  def show
+    @album = Album.find(params[:id])
+    render :show
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update(album_params)
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:band_id, :live, :name, :year)
   end
 end
