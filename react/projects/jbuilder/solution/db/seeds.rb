c2bac53822e3081fa63afbcd0c1056d785e4ba96
David Dobrynin <david.dobrynin@gmail.com>
2017-07-14 15:09:17 -0700

    Update seeds file

--- react/projects/jbuilder/solution/db/seeds.rb
+++ react/projects/jbuilder/solution/db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+john = Guest.create!(name: "John Smith", age: 45, favorite_color: "Blue")
+jane = Guest.create!(name: "Jane Doe", age: 47, favorite_color: "Green")
+josh = Guest.create!(name: "Josh Brown", age: 22, favorite_color: "Brown")
+helen = Guest.create!(name: "Helen White", age: 25, favorite_color: "White")
+job = Guest.create!(name: "Job Bluth", age: 41, favorite_color: "Red")
+
+Gift.create!(title: 'A Bottle of Wine',
+             description: "This one is not so great.",
+             guest_id: john.id)
+Gift.create!(title: 'Another Bottle of Wine',
+             description: 'This one is not so bad.',
+             guest_id: john.id)
+Gift.create!(title: 'The Hunger Games',
+             description: "A bit dark for a gift, but it's good.",
+             guest_id: jane.id)
+Gift.create!(title: 'Baseball Glove',
+             description: 'Feel like a kid again!',
+             guest_id: josh.id)
+Gift.create!(title: 'Basketball Tickets',
+             description: "The team's going 82-0 this year.",
+             guest_id: josh.id)
+Gift.create!(title: 'Hawaiian Getaway',
+             description: 'Helen has the best gifts.',
+             guest_id: helen.id)
+
+sse = Party.create!(name: "Secret Santa Extravaganza", location: "Portland")
+ccp = Party.create!(name: "Charles' Christmas Party", location: "San Francisco")
+
+Invitation.create!(guest_id: john.id, party_id: sse.id)
+Invitation.create!(guest_id: jane.id, party_id: sse.id)
+Invitation.create!(guest_id: josh.id, party_id: sse.id)
+Invitation.create!(guest_id: helen.id, party_id: sse.id)
+Invitation.create!(guest_id: job.id, party_id: sse.id)
+Invitation.create!(guest_id: josh.id, party_id: ccp.id)
+Invitation.create!(guest_id: helen.id, party_id: ccp.id)
