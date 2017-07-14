98f7f9bfea084008a21a6b14fbbe839407280399
David Dobrynin <david.dobrynin@gmail.com>
2017-07-13 20:58:38 -0700

    Create Cats controller

--- rails/projects/ninety_nine_cats_ii/skeleton/config/routes.rb
+++ rails/projects/ninety_nine_cats_ii/skeleton/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'cats/edit'
+
+  get 'cats/index'
+
+  get 'cats/new'
+
+  get 'cats/show'
+
   get 'cat_rental_requests/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
