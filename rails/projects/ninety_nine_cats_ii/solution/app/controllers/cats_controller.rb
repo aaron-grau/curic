2d98efa97b66a6215bbd857b0ac869430a6282e8
David Dobrynin <david.dobrynin@gmail.com>
2017-07-13 21:55:53 -0700

    Update Cats Controller

--- rails/projects/ninety_nine_cats_ii/solution/app/controllers/cats_controller.rb
+++ rails/projects/ninety_nine_cats_ii/solution/app/controllers/cats_controller.rb
@@ -1,4 +1,6 @@
 class CatsController < ApplicationController
+  before_action :require_user!, only: %i(new create edit update)
+
   def index
     @cats = Cat.all
     render :index
@@ -15,7 +17,7 @@ class CatsController < ApplicationController
   end
 
   def create
-    @cat = Cat.new(cat_params)
+    @cat = current_user.cats.new(cat_params)
     if @cat.save
       redirect_to cat_url(@cat)
     else
@@ -25,12 +27,12 @@ class CatsController < ApplicationController
   end
 
   def edit
-    @cat = Cat.find(params[:id])
+    @cat = current_user.cats.find(params[:id])
     render :edit
   end
 
   def update
-    @cat = Cat.find(params[:id])
+    @cat = current_user.cats.find(params[:id])
     if @cat.update_attributes(cat_params)
       redirect_to cat_url(@cat)
     else
