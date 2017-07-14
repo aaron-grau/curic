cff0bbc94e299254d520c7f03970ca8eef9986cc
Aaron Wayne <amwayne90@gmail.com>
2017-07-13 18:01:04 -0700

    Add album model

--- rails/projects/music_app/music_app/app/models/album.rb
+++ rails/projects/music_app/music_app/app/models/album.rb
@@ -1,2 +1,11 @@
 class Album < ApplicationRecord
+  belongs_to :band
+  has_many :tracks, dependent: :destroy
+
+  # can't use presence validation with boolean field
+  validates :band, :name, :year, presence: true
+  validates :live, inclusion: { in: [true, false] }
+  validates :name, uniqueness: { scope: :band_id }
+  validates :year, numericality: { minimum: 1900, maximum: 9000 }
+
 end
