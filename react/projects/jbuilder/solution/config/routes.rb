7c190c5dd613671783394a4b1512311747d02458
David Dobrynin <david.dobrynin@gmail.com>
2017-07-13 23:12:18 -0700

    Create new project

--- react/projects/jbuilder/solution/config/routes.rb
+++ react/projects/jbuilder/solution/config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    resources :guests, only: [:show, :index] do
-      resources :gifts, only: [:index]
-    end
-    resources :gifts, only: [:show]
-    resources :parties, only: [:show, :index]
-  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
