c609905be0f3f4cfa8498df53ea73ff1b7dd4c33
Julianne Costa <julesleighcosta@gmail.com>
2017-07-14 20:19:42 -0700

    prepare all migration files for migration

--- rails/projects/art_share_api/db/migrate/20170715031310_create_comments.rb
+++ rails/projects/art_share_api/db/migrate/20170715031310_create_comments.rb
@@ -1,11 +1,14 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.text :body
-      t.integer :user_id
-      t.integer :artwork_id
+      t.text :body, null: false
+      t.integer :user_id, null: false
+      t.integer :artwork_id, null: false
 
       t.timestamps
     end
+
+    add_index :comments, :user_id
+    add_index :comments, :artwork_id
   end
 end
