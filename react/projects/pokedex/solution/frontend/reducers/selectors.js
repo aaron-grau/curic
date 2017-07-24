380f094246c5759f814849ac6a00a2ac7d61f493
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-07-24 12:24:40 -0400

    Fix pokemon item detail selector

--- react/projects/pokedex/solution/frontend/reducers/selectors.js
+++ react/projects/pokedex/solution/frontend/reducers/selectors.js
@@ -7,3 +7,7 @@ export const selectPokeItems = (state, poke) => {
     poke.item_ids.map(id => state.entities.items[id]) :
     [];
 }
+
+export const selectPokemonItem = (state, id) => {
+  return state.entities.items[id];
+}
