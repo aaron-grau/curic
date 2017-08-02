c3eb130a42fd9335cf6217a676fc587abd0970b3
louiscruz <louisstephancruz@me.com>
2017-08-02 16:46:04 -0700

    And status code and rezip

--- react/projects/todos/solution_2/app/controllers/application_controller.rb
+++ react/projects/todos/solution_2/app/controllers/application_controller.rb
@@ -30,7 +30,9 @@ class ApplicationController < ActionController::Base
   end
 
   def deny_access_if_not_logged_in
-    render json: ['You must be logged in to do that'] unless logged_in?
+    unless logged_in?
+      render json: ['You must be logged in to do that'], status: :unauthorized
+    end
   end
 
   def user_params
