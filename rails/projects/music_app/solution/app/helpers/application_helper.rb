9e6220a4183a90810928209c14a7f34341a68ea2
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-08-02 14:19:17 -0400

    Finish(?) styling

--- rails/projects/music_app/solution/app/helpers/application_helper.rb
+++ rails/projects/music_app/solution/app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   def ugly_lyrics(lyrics)
-  formatted_lyrics = ""
-  lyrics.lines.each do |line|
-    formatted_lyrics << "&#9835; #{h(line)}"
-  end
+    formatted_lyrics = ""
+    lyrics.lines.each do |line|
+      formatted_lyrics << "&#9835; #{h(line)}"
+    end
 
-  "<pre>#{formatted_lyrics}</pre>".html_safe
+    "<pre class='lyrics'>#{formatted_lyrics}</pre>".html_safe
+  end
 end
 
 end
