3f848152695529a9cab86148be3cdde29b00d430
Aaron Wayne <amwayne90@gmail.com>
2017-08-01 22:18:17 -0700

    Add controllers and views

--- react/projects/bench_bnb/config/routes.rb
+++ react/projects/bench_bnb/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users/create'
+  namespace :api, defaults: {format: :json} do
+    resources :benches, only: [:index, :show, :create]
+    resources :reviews, only: [:create]
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+    resource :favorites, only: [:create, :destroy]
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#root"
 end
